7f15cfee6222467293de18e2433c89a56c56007c
Burenkovuser <nb2207@yandex.ru>
2019-09-16 17:21:49 +0700

    end

--- UIPageView/ViewController.swift
+++ UIPageView/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+               startPresentation()
+        
+    }
+
+    // Презентация при первом запуске
+    func startPresentation() {
+        
+        let userDefaults = UserDefaults.standard
+        let appAlreadeSeen = userDefaults.bool(forKey: "appAlreadeSeen")
+        if appAlreadeSeen == false {
+        
+            // Отображение PageViewController
+            if let pageViewController = storyboard?.instantiateViewController(
+                withIdentifier: "PageViewController") as? PageViewController {
+                
+                self.present(pageViewController, animated: true, completion: nil)
+            }
+        }
     }
 
 
