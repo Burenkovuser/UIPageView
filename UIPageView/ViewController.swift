98fcf16e5ead87cbb956d3e35b33805fbe66789e
Burenkovuser <nb2207@yandex.ru>
2019-09-16 11:21:39 +0700

    Initial Commit

---
+++ UIPageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPageView
+//
+//  Created by Vasilii on 16/09/2019.
+//  Copyright © 2019 Vasilii Burenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
